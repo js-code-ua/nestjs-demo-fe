f65a152b21100eb5822edc6163d06a3961fddc69
js-code-ua <js.code.ua@gmail.com>
2020-04-05 13:00:04 +0300

    minor config update

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,13 @@ import *  as _ from 'lodash';
 
 const environment = process.env.NODE_ENV || 'development';
 
-export default _.extend({
+interface IConfig {
+  environment: string;
+  BASE_URL: string;
+}
+
+export default _.extend<IConfig>({
   environment,
 },
-require(`${__dirname}/env/${environment}`), /* eslint "import/no-dynamic-require": 0 */
+require(`${__dirname}/env/${environment}`).default, /* eslint "import/no-dynamic-require": 0 */
 );
